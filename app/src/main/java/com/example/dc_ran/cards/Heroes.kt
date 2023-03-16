08076bf404d602d349d998fd9d908aaf3b271fff
owen <owenysoft@gmail.com>
2023-03-16 15:23:41 -0700

    Changes from room to json request.

---
+++ app/src/main/java/com/example/dc_ran/cards/Heroes.kt
@@ -0,0 +1,3 @@
+package com.example.dc_ran.cards
+
+class Heroes : ArrayList<HeroesItem>()
---
+++ app/src/main/java/com/example/dc_ran/cards/HeroesItem.kt
@@ -0,0 +1,7 @@
+package com.example.dc_ran.cards
+
+data class HeroesItem(
+    val name: String,
+    val tier: String,
+    val url: String
+)
